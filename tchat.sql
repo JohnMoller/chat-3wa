088469f08e957c6ff1eb52acb86c9d8b05bf9a7c
philippe-3wa <dossantos.philippe@outlook.com>
2016-06-10 12:09:26 +0200

    ok

--- tchat.sql
+++ tchat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 10 Juin 2016 à 10:34
+-- Généré le: Ven 10 Juin 2016 à 12:08
 -- Version du serveur: 5.5.47-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.14
 
@@ -33,7 +33,21 @@ CREATE TABLE IF NOT EXISTS `message` (
   `id_user` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+--
+-- Contenu de la table `message`
+--
+
+INSERT INTO `message` (`id`, `contenu`, `date`, `id_user`) VALUES
+(6, 'test', '2016-06-10 09:24:56', 1),
+(8, 'Ã§a fonctionne', '2016-06-10 09:27:08', 1),
+(9, 'coucou :)', '2016-06-10 09:30:28', 2),
+(10, 'c''est trop de la boulette', '2016-06-10 09:38:03', 1),
+(11, 'dazdazdza', '2016-06-10 09:38:26', 1),
+(12, 'eryery', '2016-06-10 10:02:27', 2),
+(13, 'adzadazdza', '2016-06-10 10:02:43', 2),
+(14, 'ehelhnkrejh', '2016-06-10 10:05:29', 2);
 
 -- --------------------------------------------------------
 
@@ -47,7 +61,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`,`password`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `password`) VALUES
+(1, 'filipe', '$2y$08$XOUblxGl0dp.LIGsl5sQDuDq/RddwrM1.aW/u2A23nCZy3Qiw1wnS'),
+(2, 'John', '$2y$08$5Q6VTeTaYfSd2nbXMCzJPeI3htH/uboRPtdq144ZSyn4baE8A/DQq');
 
 --
 -- Contraintes pour les tables exportées
