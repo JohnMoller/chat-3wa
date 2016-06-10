690df127e770a1376991386ab9fbd6885c1a014a
philippe-3wa <dossantos.philippe@outlook.com>
2016-06-10 10:15:50 +0200

    models

---
+++ models/MessageManager.class.php
@@ -0,0 +1,61 @@
+<?php
+class MessageManager
+{
+	private $link;
+
+	public function __construct($link)
+	{
+		$this->link = $link;
+	}
+	
+	public function findAll()
+	{
+		$list = [];
+		$request = "SELECT * FROM message ORDER BY id DESC";
+		$res = mysqli_query($this->link, $request);
+		while ($produit = mysqli_fetch_object($res, "Produit", [$this->link]))
+			$list[] = $produit;
+		return $list;
+	}
+
+	
+
+
+	public function create($data)
+	{
+		if (!isset($_SESSION['id']))
+			throw new Exception ("Vous devez être admin");
+
+		if (!isset($data['contenu']))
+			throw new Exception ("Missing paramater : reference");
+
+		$message = new Message($this->link);
+
+		$message->setContenu($data['contenu']);
+		$message->setIdUser();
+		
+
+		$contenu = mysqli_real_escape_string($this->link, $message->getContenu());
+		$id_user = $message->getIdUser();;
+
+		$request = "INSERT INTO message (contenu, id_user) VALUES('".$contenu."','".$id_user."')";
+		$res = mysqli_query($this->link, $request);
+		if ($res)
+		{
+			$id = mysqli_insert_id($this->link);
+			if ($id)
+			{
+				return $message;
+			}
+			else
+				throw new Exception ("Internal server error");
+		}
+		else
+			throw new Exception ("Internal server error");
+		
+	}
+
+	
+
+}
+?>
